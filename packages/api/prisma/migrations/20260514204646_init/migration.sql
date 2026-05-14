35f90c64f55778d68fe6fd176385fc8018b589f6
moyanop <pedroomoyano12@gmail.com>
2026-05-14 19:28:29 -0300

    chore(database): agrega tabla medical_certificates y migracion

---
+++ packages/api/prisma/migrations/20260514204646_init/migration.sql
@@ -0,0 +1,15 @@
+-- CreateTable
+CREATE TABLE "medical_certificates" (
+    "id" TEXT NOT NULL,
+    "member_id" TEXT NOT NULL,
+    "issue_date" DATE NOT NULL,
+    "expiry_date" DATE NOT NULL,
+    "doctor_license" TEXT NOT NULL,
+    "is_validated" BOOLEAN NOT NULL DEFAULT true,
+    "created_at" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "medical_certificates_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "medical_certificates" ADD CONSTRAINT "medical_certificates_member_id_fkey" FOREIGN KEY ("member_id") REFERENCES "members"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
