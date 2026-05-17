71ffb74824a01f03d71ead5f074d50b3cc3dc21a
Calvo-Bautista <bcalvo@alu.frlp.utn.edu.ar>
2026-05-17 20:42:05 -0300

    feat(frontend): agrega card de Sanciones en Home

--- packages/web/src/views/Home.tsx
+++ packages/web/src/views/Home.tsx
@@ -1,5 +1,5 @@
 import { Box, SimpleGrid, Heading, Text, VStack } from "@chakra-ui/react";
-import { LuUsers, LuLockKeyhole } from "react-icons/lu";
+import { LuUsers, LuLockKeyhole, LuShieldAlert } from "react-icons/lu";
 import { SectionCard } from "../components/SectionCard";
 
 export function HomeView() {
@@ -38,12 +38,19 @@ export function HomeView() {
           icon={LuLockKeyhole}
         />
 
+        <SectionCard
+          title="Sanciones"
+          description="Registra y administra las sanciones disciplinarias aplicadas a los socios del club."
+          to="/disciplines"
+          icon={LuShieldAlert}
+        />
+
         {/* Future sections can be added here following the same pattern */}
-        <Box 
-          p="6" 
-          bg="bg.muted/30" 
-          borderRadius="2xl" 
-          borderWidth="1px" 
+        <Box
+          p="6"
+          bg="bg.muted/30"
+          borderRadius="2xl"
+          borderWidth="1px"
           borderColor="border.subtle"
           borderStyle="dashed"
           display="flex"
