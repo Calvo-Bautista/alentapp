27c3d4594d8cd7b8cc667d53d35e780327fc5fcc
Bautista <bcalvo@alu.frlp.utn.edu.ar>
2026-05-30 14:07:52 -0300

    feat(discipline): se agregan 2 test de getById a getusecase

--- packages/api/src/application/GetDisciplineUseCase.test.ts
+++ packages/api/src/application/GetDisciplineUseCase.test.ts
@@ -1,13 +1,24 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
 import { GetDisciplineUseCase } from './GetDisciplineUseCase.js';
 import { DisciplineRepository } from '../domain/DisciplineRepository.js';
+import { DisciplineDTO } from '@alentapp/shared';
 
 describe('GetDisciplineUseCase', () => {
     const mockDisciplineRepo = {
         findAll: vi.fn(),
+        findById: vi.fn(),
     } as unknown as DisciplineRepository;
 
     const useCase = new GetDisciplineUseCase(mockDisciplineRepo);
+
+    const existingDiscipline: DisciplineDTO = {
+        id: "1",
+        reason: "Sancion deportiva",
+        start_date: "2026-05-23",
+        end_date: "2026-06-23",
+        is_total_suspension: true,
+        member_id: "1",
+    }
 
     beforeEach(() => {
         vi.clearAllMocks();
@@ -20,5 +31,21 @@ describe('GetDisciplineUseCase', () => {
         const result = await useCase.getAll();
         expect(result).toEqual(mockDiscipline);
         expect(mockDisciplineRepo.findAll).toHaveBeenCalledOnce();
+    });
+
+    it('debe retornar una sancion', async () => {
+        vi.mocked(mockDisciplineRepo.findById).mockResolvedValueOnce(existingDiscipline);
+
+        const result = await useCase.getById("1");
+
+        expect(result).toEqual(existingDiscipline);
+        expect(mockDisciplineRepo.findById).toHaveBeenCalledWith("1");
+    });
+
+
+    it('debe lanzar un error si la sanción no existe', async () => {
+        vi.mocked(mockDisciplineRepo.findById).mockResolvedValueOnce(null);
+
+        await expect(useCase.getById("999")).rejects.toThrow('La sanción no existe');
     });
 });
